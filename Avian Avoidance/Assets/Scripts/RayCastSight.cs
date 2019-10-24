ad6e6fd13149195dd0deedd613e1ad6af72fa6ce
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2019-10-24 16:04:12 +0100

    more raycasting
    making prefabs to test

--- Avian Avoidance/Assets/Scripts/RayCastSight.cs
+++ Avian Avoidance/Assets/Scripts/RayCastSight.cs
@@ -5,75 +5,76 @@ using UnityEngine;
 public class RayCastSight : MonoBehaviour
 {
     private Camera cam;
-    public float fireRate = .5f;
-    public Transform screenpoint;
-    public float weaponRange = 50f;
-    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
-    private LineRenderer laserLine;
-    private float nextFire;
-    Vector2 mousePos = new Vector2();
-    Event currentEvent = Event.current;
-    Vector3 point = new Vector3();
+    public GameObject prefab;
+    //public float fireRate = .5f;
+    //public Transform screenpoint;
+    public float weaponRange = 10f;
+    //private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    //private LineRenderer laserLine;
+    //private float nextFire;
+    //Vector2 mousePos = new Vector2();
+    //Event currentEvent = Event.current;
+    //Vector3 point = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        laserLine = GetComponent<LineRenderer>();
+        //laserLine = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
 
-
-    void OnGUI()
-    {
-        Vector3 point = new Vector3();
-        Event currentEvent = Event.current;
-        Vector2 mousePos = new Vector2();
-
-        // Get the mouse position from Event.
-        // Note that the y position from Event is inverted.
-        mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
-
-        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-
-        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
-        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
-        GUILayout.Label("Mouse position: " + mousePos);
-        GUILayout.Label("World position: " + point.ToString("F3"));
-        GUILayout.EndArea();
-    }
-
     private void Update()
     {
-        if (Input.GetButton("Fire1") && Time.time > nextFire)
+
+        if (Input.GetMouseButtonDown(0))
         {
-            nextFire = Time.time + fireRate;
+            Vector2 mousePos = new Vector2();
 
-            StartCoroutine(ShotEffect());
-
-            mousePos.x = currentEvent.mousePosition.x;
-            mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
-            point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
-            Vector3 RayOrigin = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+            // Get the mouse position from Event.
+            // Note that the y position from Event is inverted.
+            mousePos.x = Input.mousePosition.x;
+            mousePos.y = Input.mousePosition.y;
             RaycastHit hit;
 
-            laserLine.SetPosition(0, point);
-            if (Physics.Raycast(RayOrigin, cam.transform.forward, out hit, weaponRange))
+            Vector3 clickPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10));
+            Vector3 ScreenPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+            if (Physics.Raycast(ScreenPoint, cam.transform.forward, out hit, weaponRange))
             {
-                laserLine.SetPosition(1, hit.point);
+                Instantiate(prefab, hit.point, Quaternion.identity);
             }
             else
             {
-                laserLine.SetPosition(1, RayOrigin + (cam.transform.forward * weaponRange));
+                Instantiate(prefab, clickPoint, Quaternion.identity);
             }
         }
+
+        //if (Input.GetButton("Fire1") && Time.time > nextFire)
+        //{
+        //    nextFire = Time.time + fireRate;
+
+        //    StartCoroutine(ShotEffect());
+
+        //    point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        //    Vector3 RayOrigin = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        //    RaycastHit hit;
+
+        //    laserLine.SetPosition(0, point);
+        //if (Physics.Raycast(RayOrigin, cam.transform.forward, out hit, weaponRange))
+        //    {
+        //        laserLine.SetPosition(1, hit.point);
+        //    }
+        //    else
+        //    {
+        //        laserLine.SetPosition(1, RayOrigin + (cam.transform.forward * weaponRange));
+        //    }
+        //}
     }
 
-    private IEnumerator ShotEffect()
-    {
-        laserLine.enabled = true;
-        yield return shotDuration;
-        laserLine.enabled = false;
-    }
+    //private IEnumerator ShotEffect()
+    //{
+    //    //laserLine.enabled = true;
+    //    //yield return shotDuration;
+    //    //laserLine.enabled = false;
+    //}
 }
