8f91561c943bbc721320d1d4d1ddf85c6b15d23a
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2019-10-24 12:31:13 +0100

    Raycasting
    Working on raycasting code

--- Avian Avoidance/Assets/Scripts/RayCastSight.cs
+++ Avian Avoidance/Assets/Scripts/RayCastSight.cs
@@ -5,15 +5,25 @@ using UnityEngine;
 public class RayCastSight : MonoBehaviour
 {
     private Camera cam;
-
-
+    public float fireRate = .5f;
+    public Transform screenpoint;
+    public float weaponRange = 50f;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private LineRenderer laserLine;
+    private float nextFire;
+    Vector2 mousePos = new Vector2();
+    Event currentEvent = Event.current;
+    Vector3 point = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        laserLine = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
+
+
     void OnGUI()
     {
         Vector3 point = new Vector3();
@@ -32,5 +42,38 @@ public class RayCastSight : MonoBehaviour
         GUILayout.Label("Mouse position: " + mousePos);
         GUILayout.Label("World position: " + point.ToString("F3"));
         GUILayout.EndArea();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            mousePos.x = currentEvent.mousePosition.x;
+            mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+            point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+            Vector3 RayOrigin = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, point);
+            if (Physics.Raycast(RayOrigin, cam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+            }
+            else
+            {
+                laserLine.SetPosition(1, RayOrigin + (cam.transform.forward * weaponRange));
+            }
+        }
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
     }
 }
