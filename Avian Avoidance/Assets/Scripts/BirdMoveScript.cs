b38e1b4872f8e9d2fb2140ba72a9da7aa623fb2e
WraithTheCocoamancer <43547117+WraithTheCocoamancer@users.noreply.github.com>
2019-12-02 13:03:52 +0000

    Lerp implimentation
    added Lerp to make movement smooth having some issues with vertical movement. added toggles for the rigid bodies personal gravity to turn on and off at the start and end of movement

--- Avian Avoidance/Assets/Scripts/BirdMoveScript.cs
+++ Avian Avoidance/Assets/Scripts/BirdMoveScript.cs
@@ -1,21 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//Special thanks to third year students who helped me passing peramiters and having the bird turn I'm sorry I didn't get your names but i still wanna get you a pepsi
+//Special thanks to third year students:jack Griffiths, Aden webb & others who's name I have yet to get
 public class BirdMoveScript : MonoBehaviour
 {
 
     public Vector3 RaycastHit = Vector3.zero;
     public Vector3 BirdLocation = Vector3.zero;
     public Vector3 BirdDirection = Vector3.zero;
-
-
+    public BoxCollider Bird;
+    public Vector3 BirdTakeoff = Vector3.up;
     private RayCastFromCamera raycastfromcamera;
 
     // Start is called before the first frame update
 
     void Start()
     {
+        Bird = GetComponent<BoxCollider>();
         raycastfromcamera = GameObject.Find("Camera").GetComponent<RayCastFromCamera>();
     }
 
@@ -24,13 +25,25 @@ public class BirdMoveScript : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            transform.position = Vector3.Lerp(transform.position, transform.position + BirdTakeoff, 0.01f);
             RaycastHit = raycastfromcamera.CheckRaycast();
             //Debug.Log(RaycastHit);
             BirdLocation = transform.position;
             Debug.Log(BirdLocation);
             BirdDirection = RaycastHit - BirdLocation;
             transform.rotation = Quaternion.LookRotation(BirdDirection, Vector3.up);
-            transform.position = Vector3.MoveTowards(transform.position, RaycastHit, 100f); //to be replaced later with smooth movement.
+            Bird.attachedRigidbody.useGravity = false;
+        }
+
+        if (Vector3.Distance(transform.position, RaycastHit) >= 0.1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, RaycastHit, 0.01f); //to be replaced later with smooth movement.
+            
+        }
+        if (Vector3.Distance(transform.position, RaycastHit) <= 0.5f)
+        {
+            Bird.attachedRigidbody.useGravity = true;
+
         }
     } 
   
